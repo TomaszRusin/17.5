46d772733037947f22dceefc3f7041a65a350a5f
Tomasz Rusin <tomek@rusin.info>
2019-03-11 10:01:49 +0100

    Add node modules

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-
+var EventEmitter = require("events").EventEmitter;
 var OSinfo = require('./modules/OSinfo');
 
-process.stdin.setEncoding('utf-8');
+var emitter = new EventEmitter();
+emitter.on("beforeCommand", function (instruction) {
+    console.log('You wrote: ' + instruction + ', trying to run command');
+});
+emitter.on("afterCommand", function () {
+    console.log('Finished command');
+});
 
+process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if(input !== null) {
-        var instruction = input.toString().trim();
-        switch (instruction) {
+        var instruction = input.trim();
+        // odpalanie zdarzenia beforeCommand (z parametrem)
+        emitter.emit('beforeCommand', instruction);
+        switch(instruction) {
             case '/exit':
-                process.stdout.write('Quitting app!\n');
+                process.stdout.write('Quiting app!');
                 process.exit();
                 break;
-            case '/env':
-                var nodeVer = process.versions.node
-                var systemLang = process.env.OS
-                process.stdout.write('Node version ' + nodeVer + ' and system is ' + systemLang);
+            case '/sayhello':
+                process.stdout.write('hello!\n');
                 break;
             case '/getOSinfo':
-                OSinfo.print();
+                OSinfo.get();
                 break;
             default:
                 process.stderr.write('Wrong instruction!\n');
-        }
+        };
+        // emitowanie zdarzenia afterCommand (bez parametru)
+        emitter.emit('afterCommand');
     }
 });
-
-
-
